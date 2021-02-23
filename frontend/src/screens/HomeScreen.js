89a92584cf2e9a40a893ff4b2e7df39a31d201ec
Tim M <timothy.romero.manas@gmail.com>
2021-02-22 21:20:30 -0500

    Adding Store and Provider

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-    return (
-        <>
-            <h1>HomeScreen</h1>
-        </>
-    )
+  // Solution #2 - Using Redux
+  const dispatch = useDispatch()
+
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
+  console.log('Redux products ----- ' + JSON.stringify(products, null, 4))
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
 
 export default HomeScreen
